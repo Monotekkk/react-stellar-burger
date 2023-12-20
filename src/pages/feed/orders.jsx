9e16c7f49007f1bd2cb8633e471d24e1af56bda8
Monotek <timur.yakhin.99@gmail.com>
2023-12-20 18:54:53 +0300

    push

--- src/pages/feed/orders.jsx
+++ src/pages/feed/orders.jsx
@@ -1,33 +1,28 @@
 import style from './orders.module.css';
 import {Outlet} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
-import {WS_CONNECTION_CLOSED, WS_CONNECTION_SUCCESS, WS_GET_MESSAGE} from "../../service/actions/wsActionTypes";
-import ws from "../../utils/wsApi";
-
+import {WS_CONNECTION_START} from "../../service/actions/wsActionTypes";
+import {getUser} from '../../service/selectors/wsSekectors'
 
 function Feed() {
     const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch({type: WS_CONNECTION_SUCCESS});
-    }, []);
-    console.log(ws.onmessage)
+    const {user} = useSelector(getUser);
+    useEffect(
+        () => {
+            if (user) {
+                dispatch({type: WS_CONNECTION_START});
+            }
+        },
+        [user] // eslint-disable-line react-hooks/exhaustive-deps
+    );
     return (
         <>
             <div className={`${style.content}`}>
                 <div className={`${style} custom-scroll`}>
                     <p className={`text text_type_main-large`}>Лента заказов</p>
                     {
-                        ws.onmessage = function (event) {
-                            event.data.map((elem)=>{
-                                console.log(elem);
-                                return (
-                                    <div className={style.card}>
-                                        <p className={`${style.cardP}`}></p>
-                                    </div>
-                                )
-                            })
-                        }
+
                     }
                 </div>
                 <div></div>
